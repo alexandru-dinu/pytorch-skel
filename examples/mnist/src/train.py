5f00f7744a6dda2bc2e0c33c2c983e7dd874c02c
Alexandru Dinu <alex.dinu07@gmail.com>
2019-02-04 13:00:10 +0200

    Add TensorBoard for reporting (#1, #2).

--- examples/mnist/src/train.py
+++ examples/mnist/src/train.py
@@ -9,6 +9,8 @@ from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
+from tensorboardX import SummaryWriter
+
 import logger
 from utils import get_config, get_args, dump_cfg
 
@@ -17,8 +19,7 @@ sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../mo
 
 from mnist_model import Net
 
-
-def prologue(cfg: Namespace, *varargs) -> None:
+def prologue(cfg: Namespace, *varargs) -> SummaryWriter:
     # sanity checks
     assert cfg.device == "cpu" or (cfg.device == "cuda" and torch.cuda.is_available())
 
@@ -27,25 +28,36 @@ def prologue(cfg: Namespace, *varargs) -> None:
 
     os.makedirs(f"{base_dir}/out", exist_ok=True)
     os.makedirs(f"{base_dir}/chkpt", exist_ok=True)
+    os.makedirs(f"{base_dir}/logs", exist_ok=True)
 
     dump_cfg(f"{base_dir}/train_config.txt", vars(cfg))
 
+    # tb writer
+    writer = SummaryWriter(f"{base_dir}/logs")
+
+    return writer
+
 
 def epilogue(cfg: Namespace, *varargs) -> None:
-    pass
+    writer = varargs[0]
+    writer.close()
 
 
 def train(cfg: Namespace) -> None:
     logger.info("=== Training ===")
 
     # initial setup
-    prologue(cfg)
+    writer = prologue(cfg)
 
     model = Net()
     model.train()
+
+    test_x = torch.rand(1, 1, 28, 28)
     if cfg.device == "cuda":
         model.cuda()
+        test_x = test_x.cuda()
 
+    writer.add_graph(model, Variable(test_x, requires_grad=True))
     logger.debug("Model loaded")
 
 
@@ -57,12 +69,12 @@ def train(cfg: Namespace) -> None:
                        ])),
         batch_size=cfg.batch_size, shuffle=cfg.shuffle, num_workers=cfg.num_workers
     )
-
     logger.debug("Data loaded")
 
     optimizer = optim.Adam(model.parameters(), lr=cfg.learning_rate)
     loss_criterion = nn.NLLLoss()
     avg_loss, epoch_avg = 0.0, 0.0
+    ts = 1
 
     # train-loop
     for epoch_idx in range(cfg.start_epoch, cfg.num_epochs + 1):
@@ -88,11 +100,18 @@ def train(cfg: Namespace) -> None:
             optimizer.step()
 
             if batch_idx % cfg.batch_every == 0:
+                writer.add_scalar("train/avg_loss", avg_loss / cfg.batch_every, ts)
+
+                for name, param in model.named_parameters():
+                    writer.add_histogram(name, param, ts)
+
                 logger.debug(
                     '[%3d/%3d][%5d/%5d] avg_loss: %.8f' %
                     (epoch_idx, cfg.num_epochs, batch_idx, len(dataloader), avg_loss / cfg.batch_every)
                 )
+
                 avg_loss = 0.0
+                ts += 1
         # -- batch-loop
 
         if epoch_idx % cfg.epoch_every == 0:
@@ -101,11 +120,13 @@ def train(cfg: Namespace) -> None:
             torch.save(model.state_dict(), f"../experiments/{cfg.exp_name}/chkpt/model_{epoch_idx}.pth")
     # -- train-loop
 
+
     # save final model
     torch.save(model.state_dict(), f"../experiments/{cfg.exp_name}/model_final.pth")
 
-    # final setup
-    epilogue(cfg)
+
+    # clean-up
+    epilogue(cfg, writer)
 
 
 if __name__ == '__main__':
--- src/train.py
+++ src/train.py
@@ -9,6 +9,8 @@ from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
+from tensorboardX import SummaryWriter
+
 import logger
 from utils import get_config, get_args, dump_cfg
 
@@ -16,7 +18,7 @@ from utils import get_config, get_args, dump_cfg
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../models"))
 
 
-def prologue(cfg: Namespace, *varargs) -> None:
+def prologue(cfg: Namespace, *varargs) -> SummaryWriter:
     # sanity checks
     assert cfg.device == "cpu" or (cfg.device == "cuda" and torch.cuda.is_available())
 
@@ -25,38 +27,51 @@ def prologue(cfg: Namespace, *varargs) -> None:
 
     os.makedirs(f"{base_dir}/out", exist_ok=True)
     os.makedirs(f"{base_dir}/chkpt", exist_ok=True)
+    os.makedirs(f"{base_dir}/logs", exist_ok=True)
 
     dump_cfg(f"{base_dir}/train_config.txt", vars(cfg))
 
+    # tb writer
+    writer = SummaryWriter(f"{base_dir}/logs")
+
+    return writer
+
 
 def epilogue(cfg: Namespace, *varargs) -> None:
-    pass
+    writer = varargs[0]
+    writer.close()
 
 
 def train(cfg: Namespace) -> None:
     logger.info("=== Training ===")
 
     # initial setup
-    prologue(cfg)
+    writer = prologue(cfg)
 
-    # TODO: train-related code
+    # train-related code
     model = ...
     model.train()
+    test_x = torch.rand(...)
     if cfg.device == "cuda":
         model.cuda()
+        test_x = test_x.cuda()
 
+    writer.add_graph(model, Variable(test_x, requires_grad=True))
     logger.debug("Model loaded")
+
 
     dataset = ...
     dataloader = ...
 
     logger.debug("Data loaded")
 
+
     optimizer = ...
     loss_criterion = ...
     scheduler = ...
 
     avg_loss, epoch_avg = 0.0, 0.0
+    ts = 0
 
     # train-loop
     for epoch_idx in range(cfg.start_epoch, cfg.num_epochs + 1):
@@ -82,11 +97,18 @@ def train(cfg: Namespace) -> None:
 
             if batch_idx % cfg.batch_every == 0:
                 # do some reporting every N batches: out, running average etc
+                writer.add_scalar("train/avg_loss", avg_loss / cfg.batch_every, ts)
+
+                for name, param in model.named_parameters():
+                    writer.add_histogram(name, param, ts)
+
                 logger.debug(
                     '[%3d/%3d][%5d/%5d] avg_loss: %.8f' %
                     (epoch_idx, cfg.num_epochs, batch_idx, len(dataloader), avg_loss / cfg.batch_every)
                 )
                 avg_loss = 0.0
+                ts += 1
+
         # -- batch-loop
 
         if epoch_idx % cfg.epoch_every == 0:
@@ -99,7 +121,7 @@ def train(cfg: Namespace) -> None:
     torch.save(model.state_dict(), f"../experiments/{cfg.exp_name}/model_final.pth")
 
     # final setup
-    epilogue(cfg)
+    epilogue(cfg, writer)
 
 
 if __name__ == '__main__':
