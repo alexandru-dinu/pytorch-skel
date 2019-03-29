807adefb5b89e4ec472bdb68bf36bbb8e132cfa3
Alexandru Dinu <alex.dinu07@gmail.com>
2019-03-29 13:03:38 +0200

    (#7) Add deploy script.

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+root_dir="$1"
+
+mkdir -p $root_dir
+
+for d in configs models src;
+do
+    cp -r $d $root_dir
+done
+
+cp .gitignore $root_dir
+echo "# ..." > $root_dir/README.md
+
+echo "Created workspace in [$root_dir]"
