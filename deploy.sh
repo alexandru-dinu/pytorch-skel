8cdf655e5e27c09bc3841bd382a3a5615f09ff9e
alexandru-dinu <alex.dinu07@gmail.com>
2020-09-28 21:47:59 +0300

    Run pipreqs on deploy.

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,12 @@ clean () {
 
 clean
 
+pipreqs . --force && sed -i '' 's/==/>=/' requirements.txt
+sleep 1
+
 python setup.py sdist bdist_wheel
+sleep 1
+
 twine check dist/* && twine upload --repository pypi dist/*
 
 clean
