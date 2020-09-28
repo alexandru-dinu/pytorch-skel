399971e94a41ec736259da97183b3f8d32d423a1
alexandru-dinu <alex.dinu07@gmail.com>
2020-09-28 14:37:02 +0300

    Update requirements.

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 import re
 from setuptools import find_packages, setup
 
-# Read repository information. This will be used as the package description.
-long_description = None
+# Read repository information - use as the package description
 with open("README.md", "r") as fh:
     long_description = fh.read()
-assert long_description is not None
 
 # Read version
-version = None
 with open('bagoftools/__init__.py', 'rt') as fh:
     version = re.search(r"([0-9\.]+)", fh.read().strip()).group()
-assert version is not None
 
+# Read requirements
+with open('requirements.txt', 'rt') as fh:
+    reqs = [x.strip() for x in fh.readlines()]
 
 setup(
     name='bagoftools',
@@ -25,7 +24,7 @@ setup(
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=[],
+    install_requires=reqs,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
